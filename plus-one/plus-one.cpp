ae153fd5f886b3eea63e1d43bcdf0427802c113b
Alan Hou <alanhou911222@gmail.com>
2022-05-02 22:01:49 -0400

    Time: 0 ms, Memory: 8.9 MB - LeetHub

---
+++ plus-one/plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int vectorSize = digits.size();
+        for(int i = 0; i < vectorSize; i++){
+            if(digits[vectorSize-i-1] != 9){
+                digits[vectorSize-i-1]++;
+                return digits;
+            }
+            digits[vectorSize-i-1] = 0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
