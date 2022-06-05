b92461c746335a1a79521a737c1b56f90537b98b
Alan Hou <alanhou911222@gmail.com>
2022-06-04 23:12:46 -0400

    Time: 3 ms (35.53%), Space: 6.5 MB (33.34%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<int> empty;
+        vector<vector<int>> ans(numRows, empty);
+        for(int i = 0; i < numRows; i++){
+            int count = 0;
+            while(count < i){
+                if(count == 0){
+                    ans[i].push_back(1);
+                }
+                else{
+                    ans[i].push_back(ans[i-1][count-1]+ans[i-1][count]);
+                }
+                count++;
+            }
+            ans[i].push_back(1);
+        }
+        return ans;
+    }
+};
