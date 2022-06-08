3d68976858cf03d8d13a6b72cb557d8437987f66
Alan Hou <alanhou911222@gmail.com>
2022-06-07 22:07:23 -0400

    Time: 47 ms (98.45%), Space: 28.3 MB (5.77%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.cpp
+++ 561-array-partition-i/561-array-partition-i.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int array[20001] = {0};
+        for(int &num:nums){
+            array[num+10000]++;
+        }
+        int count = 0;
         int sum = 0;
-        for(int i = 0; i < nums.size(); i+=2){
-           sum += nums[i];
+        for(int i = 0, pick = 0; i < 20001; i++){
+            if(array[i] != 0){
+                if(pick%2 == 0){
+                    sum += i-10000;
+                    count++;
+                }
+                pick ++;
+                array[i]--;
+                i--;
+            }
         }
         return sum;
     }
