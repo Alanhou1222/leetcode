a2743bd44843a0262e667e648e505609c55d2f6a
Alan Hou <alanhou911222@gmail.com>
2022-06-14 21:07:49 -0400

    Time: 147 ms, Memory: 26.2 MB - LeetHub

---
+++ perfect-squares/perfect-squares.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        unordered_set<int> visited;
+        queue<int> q;
+        vector<int> squares;
+        int ans = 1;
+        for(int i = 1; i*i <= n; i++){
+            if(i*i == n) return ans;
+            squares.push_back(i*i);
+            visited.insert(i*i);
+            q.push(i*i);
+        }
+        while(!q.empty()){
+            ans ++;
+            int qSize = q.size();
+            for(int i = 0; i < qSize; i++){
+                for(int &square:squares){
+                    int sum = q.front() + square;
+                    if(sum == n) return ans;
+                    else{
+                        if(sum < n && !visited.count(sum)){
+                            visited.insert(sum);
+                            q.push(sum);
+                        }
+                    }
+                }
+                q.pop();
+            }
+        }
+        return -1;
+    }
+};
