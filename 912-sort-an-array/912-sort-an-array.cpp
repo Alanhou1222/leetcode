b813c07adec89427e104a9a390d5c6ce332c3b48
Alan Hou <alanhou911222@gmail.com>
2022-06-21 21:43:50 -0400

    Time: 557 ms (6.75%), Space: 156.8 MB (7.07%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        if(nums.size() <= 1) return nums;
-        vector<int> left(nums.begin(),nums.begin()+nums.size()/2);
-        vector<int> right(nums.begin()+nums.size()/2,nums.end());
-        left = sortArray(left);
-        right = sortArray(right);
-        return mergeArray(left,right);
+        mergeSort(nums, 0, nums.size()-1);
+        return nums;
     }
-    vector<int> mergeArray(vector<int>& left, vector<int>& right){
-        int leftIndex = 0, rightIndex = 0;
-        vector<int> combine;
-        while(leftIndex < left.size() && rightIndex < right.size()){
-            if(left[leftIndex] < right[rightIndex]){
-                combine.push_back(left[leftIndex++]);
-            }
-            else{
-                combine.push_back(right[rightIndex++]);
-            }
+    void mergeSort(vector<int>& nums, int l, int r){
+        if(l >= r) return;
+        mergeSort(nums,l,l+(r-l)/2);
+        mergeSort(nums,l+(r-l)/2+1,r);
+        merge(nums,l,r);
+    }
+    void merge(vector<int>& nums, int l, int r){
+        vector<int> temp;
+        int m = l+(r-l)/2;
+        int l1 = l;
+        int l2 = m+1;
+        while(l1 <= m && l2 <= r){
+            if(nums[l1] < nums[l2]) temp.push_back(nums[l1++]);
+            else temp.push_back(nums[l2++]);
         }
-        while(leftIndex < left.size()){
-            combine.push_back(left[leftIndex++]);
+        while(l1 <= m) temp.push_back(nums[l1++]);
+        while(l2 <= r) temp.push_back(nums[l2++]);
+        for(int i = l; i <= r; i++){
+            nums[i] = temp[i-l];
         }
-        while(rightIndex < right.size()){
-            combine.push_back(right[rightIndex++]);
-        }
-        return combine;
     }
 };
