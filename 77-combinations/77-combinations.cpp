65d153f8507388e7645cd7194fabb5bc4015365e
Alan Hou <alanhou911222@gmail.com>
2022-06-25 22:15:43 -0400

    Time: 7 ms (99.00%), Space: 9.1 MB (81.20%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,25 +1,20 @@
 class Solution {
-private:
-    int N;
-    int K;
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
-        N = n;
-        K = k;
         vector<int> empty;
-        backTrack(ans, empty, 1);
+        backTrack(ans, empty, 1, n, k);
         return ans;
     }
     
-    void backTrack(vector<vector<int>> &ans, vector<int> &cur, int next){
+    void backTrack(vector<vector<int>> &ans, vector<int> &cur, int next, int N, int K){
         for(int i = next; i <= N; i++){
             if(N - next >= (K - cur.size() - 1)){
                 cur.push_back(i);
                 if(cur.size() == K){
                     ans.push_back(cur);
                 }
-                else backTrack(ans, cur, i+1);
+                else backTrack(ans, cur, i+1, N, K);
                 cur.pop_back();
             }
             else{
