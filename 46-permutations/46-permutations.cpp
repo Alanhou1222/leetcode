ba3f8f4aa8c1352417bbacc1d44fb12dcc221d5e
Alan Hou <alanhou911222@gmail.com>
2022-06-27 21:40:06 -0400

    Time: 5 ms (49.46%), Space: 8 MB (28.58%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        unordered_set<int> set;
+        vector<int> empty;
+        solve(ans, set, nums, empty);
+        return ans;
+    }
+    
+    void solve(vector<vector<int>> &ans, unordered_set<int> &set, vector<int> &nums, vector<int> &cur){
+        for(int num: nums){
+            if(set.find(num) == set.end()){
+                set.insert(num);
+                cur.push_back(num);
+                if(cur.size() == nums.size()){
+                    ans.push_back(cur);
+                }
+                else{
+                    solve(ans,set,nums,cur);
+                }
+                set.erase(num);
+                cur.pop_back();
+            }
+        }
+    }
+};
