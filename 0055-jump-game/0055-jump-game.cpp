fe5eb03297556dfdbd82b2f7f766a8dcbcfb085c
Alan Hou <alanhou911222@gmail.com>
2022-10-31 15:21:35 -0400

    Time: 77 ms (88.65%), Space: 48.2 MB (99.71%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int end = 0;
+        for(int i = 0;i<nums.size()-1;i++){
+            if(nums[i]==0&&end==i){
+               return false; 
+            }
+            end = max(end,i+nums[i]);
+        }
+        return end>=nums.size()-1;
+    }
+};
