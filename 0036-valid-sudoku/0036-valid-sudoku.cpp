fa007baf3b895a01f952e99853c023886df17855
Alan Hou <alanhou911222@gmail.com>
2022-11-03 20:44:49 -0400

    Time: 42 ms (62.26%), Space: 19.5 MB (46.61%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<short int, bool> row;
+        unordered_map<short int, bool> column;
+        unordered_map<short int, bool> box;
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.'){
+                    continue;
+                }
+                if(  row[(i+1)*9+(board[i][j]-'0')] == true
+                   ||column[(j+1)*9+(board[i][j]-'0')] == true
+                   ||box[((i/3)*3+(j/3)+1)*9+(board[i][j]-'0')] == true){
+                    return false;
+                }
+                else{
+                    row[(i+1)*9+(board[i][j]-'0')] = true;
+                    column[(j+1)*9+(board[i][j]-'0')] = true;
+                    box[((i/3)*3+(j/3)+1)*9+(board[i][j]-'0')] = true;
+                }
+            }
+        }
+        return true;
+    }
+};
