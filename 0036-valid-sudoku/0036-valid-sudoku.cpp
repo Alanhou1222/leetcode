b08d89c42143903ed52c575b41eacf4d8adbfd54
Alan Hou <alanhou911222@gmail.com>
2022-11-03 20:50:11 -0400

    Time: 31 ms (81.25%), Space: 19.4 MB (48.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<short int, bool> row;
-        unordered_map<short int, bool> column;
-        unordered_map<short int, bool> box;
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
+        unordered_set<int> row;
+        unordered_set<int> column;
+        unordered_set<int> box;
+        for(short int i = 0; i < 9; i++){
+            for(short int j = 0; j < 9; j++){
                 if(board[i][j] == '.'){
                     continue;
                 }
-                if(  row[(i+1)*9+(board[i][j]-'0')] == true
-                   ||column[(j+1)*9+(board[i][j]-'0')] == true
-                   ||box[((i/3)*3+(j/3)+1)*9+(board[i][j]-'0')] == true){
+                if(row.find((i+1)*9+(board[i][j]-'0')) != row.end()  || column.find((j+1)*9+(board[i][j]-'0')) != column.end() || box.find(((i/3)*3+(j/3)+1)*9+(board[i][j]-'0')) != box.end()){
                     return false;
                 }
                 else{
-                    row[(i+1)*9+(board[i][j]-'0')] = true;
-                    column[(j+1)*9+(board[i][j]-'0')] = true;
-                    box[((i/3)*3+(j/3)+1)*9+(board[i][j]-'0')] = true;
+                    row.insert((i+1)*9+(board[i][j]-'0'));
+                    column.insert((j+1)*9+(board[i][j]-'0'));
+                    box.insert(((i/3)*3+(j/3)+1)*9+(board[i][j]-'0'));
                 }
             }
         }
